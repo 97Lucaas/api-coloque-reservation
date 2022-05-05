bcc0041452331cb525b80ad1745adcdade672c18
Arthaud Proust <arthaudp33@gmail.com>
2022-05-05 16:01:39 +0200

    add invitation model and auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\InvitationsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::resource('invitations', InvitationsController::class);
+
+require __DIR__.'/auth.php';
