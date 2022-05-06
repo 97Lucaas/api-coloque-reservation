85ec29a07983554abea1c6f770263500cdab28e3
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2022-05-06 17:02:23 +0200

    margin

--- resources/views/invitations/index.blade.php
+++ resources/views/invitations/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
             @foreach($invitations as $invitation)
-                <article class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <article class="bg-white overflow-hidden shadow-sm sm:rounded-lg m-3">
                     <header class="p-6">
                         <h4 class="text-3xl mb-3">{{ $invitation->full_name()}} @if($invitation->is_scanned)(scanné)@endif</h4>
 
