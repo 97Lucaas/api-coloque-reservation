fa3bc8c62c3a42372d4ae46224ddcfdd42a2a490
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2022-05-22 18:25:48 +0200

    mail date display

--- app/Mail/InvitationCreated.php
+++ app/Mail/InvitationCreated.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
+use function PHPUnit\Framework\isNull;
+
 class InvitationCreated extends Mailable
 {
     use Queueable, SerializesModels;
@@ -34,14 +36,21 @@ class InvitationCreated extends Mailable
         $full_name = $this->invitation->full_name();
 
         $event_name = $this->invitation->event->title;
+
         $event_start_date = $this->invitation->event->start_date_humanized;
+
+        if(!is_null($this->invitation->event->start_date)){
+            $datesentence = " le **$event_start_date**,";
+        }else{
+            $datesentence = "";
+        }
 
         return $this->markdown('vendor.notifications.invitation')
             ->subject("Votre ticket pour $event_name")
             ->with('key',$this->invitation->key)
             ->with('introLines', [
                 "Bonjour **$full_name**, voici votre billet pour **$event_name** !",
-                "Nous vous le demanderons le **$event_start_date**, le jour de l'évènement."
+                "Nous vous le demanderons$datesentence le jour de l'évènement."
             ])
         ;
     }
